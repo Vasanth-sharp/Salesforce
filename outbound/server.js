de6b441dcfd5e0546509829b61b3055ba06010fa
Vasanthakumar <118686684+Vasanth-sharp@users.noreply.github.com>
2024-12-02 22:58:39 +0530

    Add files via upload

---
+++ outbound/server.js
@@ -0,0 +1,21 @@
+const express=require("express")
+const xml2js=require("xml2js")
+const bodyparser=require("body-parser")
+const app=express()
+
+app.use(bodyparser.text({type:"application/xml"}))
+
+app.listen(4000,()=>console.log("I am listening..."))
+
+app.post("/api/xml",(req,res)=>{
+    const xmlData=req.body;
+
+    xml2js.parseString(xmlData,(err,result)=>{
+        if(err) return res.status(400).send("Invalid xml");
+
+        console.log(result)
+
+        res.status(200).send("XML received")
+    })
+})
+
